733d417d9e9c0196bc45005ce856e02ec63b9e5b
Fererra <126345645+Fererra@users.noreply.github.com>
2024-10-27 07:48:53 +0200

    completed task

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => (x) => null;
+const pipe = (...fns) => {
+    for (const fn of fns) {
+        if (typeof fn !== 'function') {
+            throw new Error('All arguments should be a functions');
+        }
+    }
+    return (x) => fns.reduce((value, fn) => fn(value), x);
+};
 
 module.exports = { pipe };
